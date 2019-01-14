ec345c72206e3b303ecf6072e3d6ec61627d9033
Hannu Kärkkäinen <hannu@redandblue.fi>
2019-01-14 09:27:50 +0200

    Have Header test use TestWrapper

--- src/components/Header/Header.spec.js
+++ src/components/Header/Header.spec.js
@@ -1,18 +1,12 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import { MemoryRouter } from 'react-router-dom';
-import { IntlProvider } from 'react-intl';
+import TestWrapper from '../../../test/TestWrapper';
 import HeaderComponent from './Header';
 
-// We need to wrap the original Header with 'MemoryRouter'
-// because Header uses Link, which relies on React-Router context
-const messages = {};
 const Header = () => (
-  <MemoryRouter>
-    <IntlProvider locale="en" messages={messages}>
-      <HeaderComponent />
-    </IntlProvider>
-  </MemoryRouter>
+  <TestWrapper>
+    <HeaderComponent />
+  </TestWrapper>
 );
 
 it('renders Header correctly', () => {
